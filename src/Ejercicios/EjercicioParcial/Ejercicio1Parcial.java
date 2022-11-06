e9aac4cb7c63baf7222b906342a561a0ddf1a1ea
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-11-06 00:36:02 -0300

    cambios

--- src/Ejercicios/EjercicioParcial/Ejercicio1Parcial.java
+++ src/Ejercicios/EjercicioParcial/Ejercicio1Parcial.java
@@ -9,39 +9,37 @@ package Ejercicios.EjercicioParcial;
  * @author galin
  */
 public class Ejercicio1Parcial {
+
     public static void main(String[] args) {
-        int i=0,j=0,posFila;
-        int Matriz[][]= 
-            {{1,2,3},
-             {4,5,6},
-             {7,7,9}};
-        int  Fila[]= new int[Matriz[0].length];
-        
-        posFila=RecorrerMatriz(i,j,Matriz);
-        System.out.println(posFila);
+        int i = 0, j = 0, posFila;
+        posFila = -1;
+        int Matriz[][]
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 8}};
+
+        System.out.println(RecorrerMatriz(i, j, Matriz, posFila));
     }
-    
-    public static int RecorrerMatriz(int i,int j,int Matriz[][]){
-        int posFila;
-        posFila=-1;
-        
-        if(i!=Matriz.length-1 || j!=Matriz.length-1){
-            
-            if(j<Matriz.length){
-                System.out.println(Matriz[i][j]);
-                RecorrerMatriz(i,j+1,Matriz);
-            }else{
-                RecorrerMatriz(i+1,0,Matriz);
+        //esta bien que posFila entre por parametros
+ 
+
+    public static int RecorrerMatriz(int fila, int colum, int Matriz[][], int posFila) {
+
+        if (fila != Matriz.length - 1 || colum != Matriz[0].length - 1) {
+
+            if (colum < Matriz.length - 1) {
+                if (Matriz[fila][colum] == Matriz[fila][colum + 1]) {
+                    posFila = fila;
+                } else {
+                    posFila = RecorrerMatriz(fila, colum + 1, Matriz, posFila);
+                }
+            } else {
+                if (posFila == -1) {
+                    posFila = RecorrerMatriz(fila + 1, 0, Matriz, posFila);
+                }
             }
-        }else{
-            System.out.println(Matriz[i][j]);
         }
-        
         return posFila;
     }
-    public static void VerificarFila(){
-        if(){
-            
-        }
-    }
+
 }
