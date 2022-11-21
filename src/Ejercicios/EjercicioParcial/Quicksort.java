30df341b9edab00ec0407252394655503eeec614
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-11-21 01:17:46 -0300

    quicksort

---
+++ src/Ejercicios/EjercicioParcial/Quicksort.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios.EjercicioParcial;
+
+/**
+ *
+ * @author galin
+ */
+public class Quicksort {
+    public static void main(String[] args) {
+            int arr[]={4,5,1,2},i=0,j=0;
+            
+            
+    }
+}
